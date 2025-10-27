c01734b4e0b6984b7a26a4c59868dab87d15c8a3
Val073-ops <valeryjuru73@gmail.com>
2025-10-27 07:07:52 +0200

    Update Guess the number game.py
    Track number of user attempts in guessing game

--- Guess the number game.py
+++ Guess the number game.py
@@ -1,17 +1,20 @@
 import random
 number = random.randint(1, 50)
 guess = None
+attempt = 0
 
 print("Hey you lets play the 'guess the number' game!!!")
 while guess != number:
     try:
         guess = int(input("Guess a number between 1 and 50: "))
+        attempt = attempt + 1
 
         if guess < number:
             print("Too low, try again!!")
         elif guess > number:
             print("Too high, try again!!")
         else:
-            print(f"Congrats you finally guessed the number!!")
+            print(f"Congrats you finally guessed the number on {attempt} attempts!!")
     except ValueError:
+
         print("Enter a valid number!")
